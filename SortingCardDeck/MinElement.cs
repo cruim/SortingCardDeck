8d95e911b81fb4e081d853e542317e23f8f10175
Daniil <hedgehog147@gmail.com>
2016-07-31 13:51:47 +0300

    init commit

---
+++ SortingCardDeck/MinElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public class MinElement
+{
+    public int SortingCardDeck(List<int> array)
+    {
+        int minElement = array[0];
+        for (int i = 0; i < array.Count; i++)
+        {
+            if (minElement > array[i])
+            {
+                minElement = array[i];
+            }
+        }
+
+        return minElement;
+    }
+}
+
--- SortingCardDeck/Program.cs
+++ SortingCardDeck/Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SortingCardDeck
+class Solution
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Random rnd = new Random();
+
+        int i = Convert.ToInt32(Console.ReadLine()); // начальное число элементов
+        int[] array = new int[i];
+
+        // экземпляр класса, отвечающий за поиск минимального элемента
+        MinElement minEl = new MinElement();
+
+
+        for (int j = 0; j < array.Length; j++)
         {
+            array[j] = j + 1;
+            Console.Write(array[j] + " ");
+
         }
+        Console.Write("\n");
+
+        array = array.OrderBy(n => rnd.Next()).ToArray(); // перешивание элементов массива
+
+        foreach (int t in array)
+        {
+            Console.Write(t + " ");
+        }
+
+        // в первый лист копируем эдементы массива
+        List<int> firstList = new List<int>(array);
+
+        // во второй складываем элементы первого по возрастанию, 
+        // удаляя каждый раз минимальный из первого листа
+        List<int> secondList = new List<int>(); 
+        
+
+        for (var j = 0; j < i; j++)
+        {
+            var minElement = minEl.SortingCardDeck(firstList);
+
+            secondList.Add(minElement);
+
+            firstList.Remove(minElement);
+        }
+
+        Console.Write("\n");
+        foreach (int t in secondList)
+        {
+            Console.Write(t + " ");
+        }
+
+        Console.ReadLine();
+
     }
+
+
 }
+
